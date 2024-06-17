abede9b808a65974749b081628c8f27ec5f279e2
Ayush Kumar <theofficialayush.kumar@gmail.com>
2024-06-17 16:16:41 +0530

    feat: dark mode added to components

--- app/components/ui/card-list.tsx
+++ app/components/ui/card-list.tsx
@@ -39,7 +39,7 @@ const cardContents: CardContent[] = [
 const Card = ({ content }: { content: CardContent }) => {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[25rem] h-auto rounded-xl p-6 border m-auto">
+      <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2] w-auto sm:w-[25rem] h-auto rounded-xl p-6 border m-auto">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -49,7 +49,7 @@ const Card = ({ content }: { content: CardContent }) => {
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className=" text-sm max-w-sm mt-2 text-neutral-300"
         >
           {content.description}
         </CardItem>
@@ -68,7 +68,7 @@ const Card = ({ content }: { content: CardContent }) => {
             as={Link}
             href={content.link}
             target="__blank"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+            className="px-4 py-2 rounded-xl text-xs font-normal text-white"
           >
             {content.buttonText}
           </CardItem>
