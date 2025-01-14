a97bdebc10b50bf36e0abe520c757166435a26bc
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-01-14 13:41:30 +0530

    fix: form push

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -4,9 +4,9 @@ const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) return;
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    // console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    // console.error('MongoDB connection error:', error);
   }
 };
 
