105245df2e632047a49b4f2d88ee20bb08830a6a
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-01-14 12:36:22 +0530

    fix: backend for deployment

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
